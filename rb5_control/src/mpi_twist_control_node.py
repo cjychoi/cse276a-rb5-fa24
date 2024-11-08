be4d5f55620e2a60d237016dd723d5ef2f7807d7
NChidambaram06 <57117575+NChidambaram06@users.noreply.github.com>
2024-11-07 22:06:04 -0800

    Update mpi_twist_control_node.py

--- rb5_control/src/mpi_twist_control_node.py
+++ rb5_control/src/mpi_twist_control_node.py
@@ -1,14 +1,3 @@
-#!/usr/bin/env python3
-""" MegaPi Controller ROS2 Wrapper"""
-# import rospy
-import rclpy  # replaces rospy
-from rclpy.node import Node
-
-from geometry_msgs.msg import Twist
-from mpi_control import MegaPiController
-import numpy as np
-
-
 class MegaPiControllerNode(Node):
     def __init__(self, verbose=True, debug=False):
         super().__init__("megapi_controller_node")
@@ -16,7 +5,7 @@ class MegaPiControllerNode(Node):
         self.r = 0.025  # radius of the wheel
         self.lx = 0.055  # half of the distance between front wheel and back wheel
         self.ly = 0.07  # half of the distance between left wheel and right wheel
-        self.calibration = 1.0
+        self.calibration = 0.25  # Reduce calibration to 0.25 for 4x movement
         self.subscription = self.create_subscription(
             Twist, "/twist", self.twist_callback, 10
         )
@@ -46,16 +35,3 @@ class MegaPiControllerNode(Node):
         self.mpi_ctrl.setFourMotors(
             int(result[0][0]), int(result[1][0]), int(result[2][0]), int(result[3][0])
         )
-
-
-if __name__ == "__main__":
-    rclpy.init()
-    mpi_ctrl_node = MegaPiControllerNode()
-    # rospy.init_node('megapi_controller')
-    # rospy.Subscriber('/twist', Twist, mpi_ctrl_node.twist_callback, queue_size=1)
-
-    rclpy.spin(mpi_ctrl_node)  # Spin for until shutdown
-
-    # Destroy node and shutdown when done. (Optional, as node would be cleaned up by garbage collection)
-    mpi_ctrl_node.destroy_node()
-    rclpy.shutdown()
