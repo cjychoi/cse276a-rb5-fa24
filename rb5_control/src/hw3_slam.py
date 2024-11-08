98f8ec479aeb7455a958d933e829c1fa48317066
NChidambaram06 <57117575+NChidambaram06@users.noreply.github.com>
2024-11-07 20:48:40 -0800

    Update hw3_slam.py

--- rb5_control/src/hw3_slam.py
+++ rb5_control/src/hw3_slam.py
@@ -65,7 +65,9 @@ class YoloCameraNode(Node):
 
         self.objects_to_detect = {
             'teddy bear': 0.2,
-            'backpack': 0.2
+            'backpack': 0.2,
+            'umbrella': 0.6,
+            'bottle': 0.1
         }
         self.detected_objects = set()  # Track detected objects
         self.detection_timeout = 10
@@ -148,7 +150,7 @@ class YoloCameraNode(Node):
     def start_rotation(self):
         # Rotate the robot 360 degrees to track objects
         rotate_twist = Twist()
-        rotate_twist.angular.z = 0.5  # Set a slow rotation speed
+        rotate_twist.angular.z = 5.0  # Set a slow rotation speed
         self.rotation_start_time = time.time()
         self.rotation_duration = 2 * np.pi / 0.5  # Total time to spin 360 degrees at given angular speed
 
