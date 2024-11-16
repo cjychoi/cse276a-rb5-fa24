a30a66a7355a71b5db3a1f13f1b27ce8e857135f
NChidambaram06 <57117575+NChidambaram06@users.noreply.github.com>
2024-11-15 20:33:12 -0800

    Create hw3_move.py

---
+++ rb5_control/src/hw3_move.py
@@ -0,0 +1,82 @@
+# hw3_slam.py - Adjusted to calculate object positions relative to the robot's current position and update EKF SLAM
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float32MultiArray
+from geometry_msgs.msg import Twist
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+class MovementCommands(Node):
+    def __init__(self):
+        super().__init__('movement_commands_node')
+        
+        self.spin_and_track_pub = self.create_publisher(Float32MultiArray, '/start_spin_and_track', 10)
+        
+        self.update_and_plot_pub = self.create_publisher(Bool, '/start_update_and_plot', 10)
+        
+        self.plot_final_landmarks_pub = self.create_publisher(Bool, '/start_plot_final_landmarks', 10)
+
+
+    def spin_and_track(self, type, distance):
+        if type == 'move:
+            msg = Float32MultiArray()
+            msg.data = [1.0, distance]
+            self.spin_and_track_pub.publish(msg)
+        else:
+            msg = Float32MultiArray()
+            msg.data = [2.0, distance]
+            self.spin_and_track_pub.publish(msg)
+
+    def update_and_plot(self):
+        msg = Bool()
+        msg.data = True
+        self.update_and_plot_pub.publish(msg)
+
+    def plot_final_landmarks(self):
+        msg = Bool()
+        msg.data = True
+        self.plot_final_landmarks_pub.publish(msg)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MovementCommands()
+  
+    print("SLAM 1")
+
+    node.spin_and_track('move', 0.0)
+    time.sleep(1)
+
+    # TRY 1
+    # Square movement
+    for _ in range(1):
+        for _ in range(4):  # Stop every 0.5 meters
+            print("SLAM loop")
+            node.spin_and_track('move', 0.5)
+            time.sleep(1)
+        node.spin_and_track('spin', 90)
+        time.sleep(1)
+
+    node.update_and_plot()
+    node.plot_final_landmarks()
+
+    # node.spin_and_track('move', 0.5)
+    # time.sleep(1)
+
+    # Octogon movement
+    # for _ in range(8):
+    #     for _ in range(2):  # Stop every 0.5 meters
+    #         print("SLAM loop")
+    #         node.spin_and_track('move', 0.5)
+    #         time.sleep(1)
+    #     node.spin_and_track('spin', 45)
+    #     time.sleep(1)
+        
+
+
+
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
